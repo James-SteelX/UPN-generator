d43948f44154f266f06cbb099f15def09c8ba79e
James Steel <james@Jamess-MacBook-Pro.local>
2017-05-12 08:33:18 +0100

    minor formatting and refactor

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,22 +1,22 @@
 class Generator
 
   LETTERS = {
-    0 => 'A', 1 => 'B', 2 => 'C', 3 => 'D', 4 => 'E', 5 => 'F', 6 => 'G',
-    7 => 'H', 8 => 'J', 9 => 'K', 10 => 'L',11 => 'M',12 => 'N',13 => 'P',
-    14 => 'Q',15 => 'R',16 => 'T',17 => 'U',18 => 'V',19 => 'W',20 => 'X',
-    21 => 'Y',22 => 'Z'
+    0 => 'A', 1 => 'B', 2 => 'C', 3 => 'D', 4 => 'E', 5 => 'F',
+    6 => 'G', 7 => 'H', 8 => 'J', 9 => 'K', 10 => 'L', 11 => 'M',
+    12 => 'N', 13 => 'P', 14 => 'Q', 15 => 'R', 16 => 'T', 17 => 'U',
+    18 => 'V', 19 => 'W', 20 => 'X', 21 => 'Y', 22 => 'Z'
   }
 
 # Validate a UPN is correct.
   def validate(upn)
     array = upn.split(//)
-    firstCharacter = array.slice!(0)
+    first_character = array.slice!(0)
 
-    if LETTERS.has_value?(firstCharacter) && array.length == 12
-      total = array.map.with_index{ |element, index| element.to_i * (index + 2)}
+    if LETTERS.has_value?(first_character) && array.length == 12
+      total = array.map.with_index { |el, ind| el.to_i * (ind + 2) }
       key = total.inject(:+).to_f % 23
 
-      LETTERS[key.floor] == firstCharacter
+      LETTERS[key.floor] == first_character
     else
       false
     end
@@ -25,7 +25,7 @@ class Generator
 # Generate a valid UPN.
   def generate
     array = Array.new(12) { rand(1...9) }
-    totals = array.map.with_index{ |element, index| element.to_i * (index + 2)}
+    totals = array.map.with_index { |el, ind| el.to_i * (ind + 2) }
     key = totals.inject(:+).to_f % 23.0
 
     upn = array.unshift(LETTERS[key.floor])
@@ -41,11 +41,9 @@ class Generator
     while $i < amount do
       upn = generate
 
-      if upns.include?(upn) || !validate(upn)
-        redo
-      end
+      redo if upns.include?(upn) || !validate(upn)
 
-      upns.push(upn)
+      upns << upn
 
       $i += 1
     end
